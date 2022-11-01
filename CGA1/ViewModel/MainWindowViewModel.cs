7b804e35e06e833395d8169e3c7234a600cb28c7
ZaharPro <zahar7331@gmail.com>
2022-11-01 14:17:23 +0300

    add ViewModel

---
+++ CGA1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CGA1.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, newValue))
+            {
+                return false;
+            }
+            field = newValue;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
