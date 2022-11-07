cb2430d10dd36eb31e2504b863585b4463e32f61
ZaharPro <zahar7331@gmail.com>
2022-11-07 18:27:09 +0300

    add ObjParser

--- CGA1/Model/ObjParser.cs
+++ CGA1/Model/ObjParser.cs
@@ -1,104 +1,102 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Numerics;
-using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
 
 namespace CGA1.Model
 {
-    public static class ObjParsers
+    public class ObjParser
     {
-        private static bool IsPresent(string s) => !string.IsNullOrEmpty(s);
-
-        private static float ParseFloat(string s) => float.Parse(s, CultureInfo.InvariantCulture);
-
-        private static R NextOrElse<T, R>(IEnumerator<T> e, Predicate<T> predicate, Func<T, R> mapper, R value = default)
+        public Obj Parse(TextReader reader)
         {
-            return e.MoveNext() && predicate(e.Current) ? mapper(e.Current) : value;
-        }
+            var vertices = new List<Vector4>();
+            var textures = new List<Vector3>();
+            var normals = new List<Vector3>();
+            var faces = new List<IList<Vector3>>();
 
-        private static Vector3 ParseVector3(IEnumerable<string> values, Vector3 defaultValue = default)
-        {
-
-            using (var e = values.GetEnumerator())
+            string line;
+            while ((line = reader.ReadLine()) != null)
             {
-                var x = NextOrElse(e, IsPresent, ParseFloat, defaultValue.X);
-                var y = NextOrElse(e, IsPresent, ParseFloat, defaultValue.Y);
-                var z = NextOrElse(e, IsPresent, ParseFloat, defaultValue.Z);
-                return new Vector3(x, y, z);
+                var strings = line.Split(' ');
+                if (strings.Length == 0)
+                    continue;
+                var type = strings[0];
+                var args = strings.Skip(1);
+                switch (type)
+                {
+                    case "v":
+                        var vertex = ParseVertex(args);
+                        vertices.Add(vertex);
+                        break;
+                    case "vt":
+                        var texture = ParseTexture(args);
+                        textures.Add(texture);
+                        break;
+                    case "vn":
+                        var normal = ParseNormal(args);
+                        normals.Add(normal);
+                        break;
+                    case "f":
+                        var face = args
+                            .Select(f => ParseFace(f.Split('/')) - Vector3.One)
+                            .ToList();
+                        faces.Add(face);
+                        break;
+                }
             }
+            return new Obj(vertices, textures, normals, faces);
         }
 
-        private static Vector4 ParseVector4(IEnumerable<string> values, Vector4 defaultValue = default)
+        private Vector4 ParseVertex(IEnumerable<string> args)
         {
-
-            using (var e = values.GetEnumerator())
+            using (var e = args.GetEnumerator())
             {
-                var x = NextOrElse(e, IsPresent, ParseFloat, defaultValue.X);
-                var y = NextOrElse(e, IsPresent, ParseFloat, defaultValue.Y);
-                var z = NextOrElse(e, IsPresent, ParseFloat, defaultValue.Z);
-                var w = NextOrElse(e, IsPresent, ParseFloat, defaultValue.W);
+                var x = e.MoveNext() ? ParseFloat(e.Current) : throw new Exception();
+                var y = e.MoveNext() ? ParseFloat(e.Current) : throw new Exception();
+                var z = e.MoveNext() ? ParseFloat(e.Current) : throw new Exception();
+                var w = e.MoveNext() ? ParseFloat(e.Current) : 1.0f;
                 return new Vector4(x, y, z, w);
             }
         }
 
-        public static Obj Parse(string path)
+        private Vector3 ParseTexture(IEnumerable<string> args)
         {
-            if (!File.Exists(path))
+            using (var e = args.GetEnumerator())
             {
-                throw new ArgumentException("File doesn't exists");
+                var x = e.MoveNext() ? ParseFloat(e.Current) : throw new Exception();
+                var y = e.MoveNext() ? ParseFloat(e.Current) : throw new Exception();
+                var z = e.MoveNext() ? ParseFloat(e.Current) : 0.0f;
+                return new Vector3(x, y, z);
             }
-            var points = new ConcurrentQueue<Vector4>();
-            var textures = new ConcurrentQueue<Vector3>();
-            var normals = new ConcurrentQueue<Vector3>();
-            var faces = new ConcurrentQueue<IList<Vector3>>();
+        }
 
-            var readLinesBlock = new TransformManyBlock<string, string>(File.ReadLines);
-            var splitLineBlock = new TransformBlock<string, string[]>(line => line.Split(' '));
-            var pointParser = new ActionBlock<string[]>(strings =>
+        private Vector3 ParseNormal(IEnumerable<string> args)
+        {
+            using (var e = args.GetEnumerator())
             {
-                var args = strings.Skip(1);
-                var point = ParseVector4(args, new Vector4(0, 0, 0, 1));
-                points.Enqueue(point);
-            });
-            var textureParser = new ActionBlock<string[]>(strings =>
+                var x = e.MoveNext() ? ParseFloat(e.Current) : throw new Exception();
+                var y = e.MoveNext() ? ParseFloat(e.Current) : throw new Exception();
+                var z = e.MoveNext() ? ParseFloat(e.Current) : throw new Exception();
+                return new Vector3(x, y, z);
+            }
+        }
+
+        private Vector3 ParseFace(IEnumerable<string> args)
+        {
+            using (var e = args.GetEnumerator())
             {
-                var args = strings.Skip(1);
-                var texture = ParseVector3(args);
-                textures.Enqueue(texture);
-            });
-            var normalParser = new ActionBlock<string[]>(strings =>
-            {
-                var args = strings.Skip(1);
-                var normal = ParseVector3(args);
-                textures.Enqueue(normal);
-            });
-            var facesParser = new ActionBlock<string[]>(strings =>
-            {
-                var args = strings.Skip(1);
-                var face = args.Select(f => ParseVector3(f.Split('/'))).ToList();
-                faces.Enqueue(face);
-            });
+                var x = e.MoveNext() ? int.Parse(e.Current) : throw new Exception();
+                var y = e.MoveNext() && !string.IsNullOrEmpty(e.Current) ? int.Parse(e.Current) : 0;
+                var z = e.MoveNext() && !string.IsNullOrEmpty(e.Current) ? int.Parse(e.Current) : 0;
+                return new Vector3(x, y, z);
+            }
+        }
 
-            readLinesBlock.LinkTo(splitLineBlock);
-            splitLineBlock.LinkTo(pointParser, strigs => "v".Equals(strigs[0]));
-            splitLineBlock.LinkTo(textureParser, strigs => "vt".Equals(strigs[0]));
-            splitLineBlock.LinkTo(normalParser, strigs => "vn".Equals(strigs[0]));
-            splitLineBlock.LinkTo(facesParser, strigs => "f".Equals(strigs[0]));
-
-            readLinesBlock.Post(path);
-
-            var completion = Task.WhenAll(pointParser.Completion,
-                textureParser.Completion,
-                normalParser.Completion,
-                facesParser.Completion);
-            completion.GetAwaiter().GetResult();
-
-            return new Obj(points.ToList(), textures.ToList(), normals.ToList(), faces.ToList());
+        private float ParseFloat(string s)
+        {
+            return float.Parse(s, CultureInfo.InvariantCulture);
         }
     }
 }
