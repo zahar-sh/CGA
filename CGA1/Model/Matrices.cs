6edab3acfc6f1bef8c5df91551d277a51e77f0cb
ZaharPro <zahar7331@gmail.com>
2022-11-08 23:48:42 +0300

    update Matrices

--- CGA1/Model/Matrices.cs
+++ CGA1/Model/Matrices.cs
@@ -1,54 +1,138 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 namespace CGA1.Model
 {
     public static class Matrices
     {
-        public static Matrix4x4 CreateWorldMatrix(Vector3 translation, float scale, float yaw, float pitch, float roll)
+        public static Matrix4x4 CreateTranslation(Vector3 v)
         {
-            return Matrix4x4.CreateTranslation(translation) *
-                Matrix4x4.CreateScale(scale) *
-                Matrix4x4.CreateFromYawPitchRoll(yaw, pitch, roll);
+            return CreateTranslation(v.X, v.Y, v.Z);
         }
 
-        public static Matrix4x4 CreateViewerMatrix(Vector3 camera, float yaw, float pitch, float roll)
+        public static Matrix4x4 CreateTranslation(float x, float y, float z)
         {
-            return Matrix4x4.CreateTranslation(-camera) *
-                Matrix4x4.Transpose(Matrix4x4.CreateFromYawPitchRoll(yaw, pitch, roll));
-        }
-
-        public static Matrix4x4 CreateWorldProjectionMatrix(float fieldOfView, float aspectRatio, float nearPlaneDistance, float farPlaneDistance)
-        {
-            return Matrix4x4.CreatePerspectiveFieldOfView(fieldOfView, aspectRatio, nearPlaneDistance, farPlaneDistance);
-        }
-
-        public static Matrix4x4 CreateViewportMatrix(int minX, int minY, int width, int height)
-        {
-            var num = width / 2;
-            var num2 = height / 2;
             return new Matrix4x4(
-                num, 0, 0, minX + num,
-                0, -num2, 0, minY + num2,
+                1, 0, 0, x,
+                0, 1, 0, y,
+                0, 0, 1, z,
+                0, 0, 0, 1);
+        }
+
+        public static Matrix4x4 CreateScale(float scale)
+        {
+            return new Matrix4x4(
+                scale, 0, 0, 0,
+                0, scale, 0, 0,
+                0, 0, scale, 0,
+                0, 0, 0, 1);
+        }
+
+        public static Matrix4x4 CreateRotationX(float angle)
+        {
+            var sin = (float)Math.Sin(angle);
+            var cos = (float)Math.Cos(angle);
+            return new Matrix4x4(
+                1, 0, 0, 0,
+                0, cos, -sin, 0,
+                0, sin, cos, 0,
+                0, 0, 0, 1);
+        }
+
+        public static Matrix4x4 CreateRotationY(float angle)
+        {
+            var sin = (float)Math.Sin(angle);
+            var cos = (float)Math.Cos(angle);
+            return new Matrix4x4(
+                cos, 0, sin, 0,
+                0, 1, 0, 0,
+                -sin, 0, cos, 0,
+                0, 0, 0, 1);
+        }
+
+        public static Matrix4x4 CreateRotationZ(float angle)
+        {
+            var sin = (float)Math.Sin(angle);
+            var cos = (float)Math.Cos(angle);
+            return new Matrix4x4(
+                cos, -sin, 0, 0,
+                sin, cos, 0, 0,
                 0, 0, 1, 0,
                 0, 0, 0, 1);
         }
 
-        public static void Transform(Obj obj, Matrix4x4 worldMatrix, Matrix4x4 cameraMatrix, Matrix4x4 worldProjectionMatrix, Matrix4x4 viewportMatrix)
+        public static Matrix4x4 CreateRotation(Vector3 v)
         {
-            var matrix = worldMatrix * cameraMatrix * worldProjectionMatrix;
-            var vertices = obj.Vertices;
-            for (int i = 0; i < vertices.Count; i++)
-            {
-                var v = vertices[i];
-                var v1 = Vector4.Transform(Vector4.Transform(v, matrix) / v.W, viewportMatrix);
-                v1.W = v.W;
-                vertices[i] = v1;
-            }
-            var normals = obj.Normals;
-            for (int i = 0; i < normals.Count; i++)
-            {
-                normals[i] = Vector3.Normalize(Vector3.TransformNormal(normals[i], worldMatrix));
-            }
+            return CreateRotation(v.X, v.Y, v.Z);
+        }
+
+        public static Matrix4x4 CreateRotation(float angleX, float angleY, float angleZ)
+        {
+            return CreateRotationX(angleX) *
+                CreateRotationY(angleY) *
+                CreateRotationZ(angleZ);
+        }
+
+        public static Matrix4x4 CreateModelMatrix(Vector3 translation, Vector3 rotation, float scale)
+        {
+            return CreateTranslation(translation) *
+                CreateRotation(rotation) *
+                CreateScale(scale);
+        }
+
+        public static Matrix4x4 CreateModelMatrix(float x, float y, float z, float angleX, float angleY, float angleZ, float scale)
+        {
+            return CreateTranslation(x, y, z) *
+                CreateRotation(angleX, angleY, angleZ) *
+                CreateScale(scale);
+        }
+
+        public static Matrix4x4 CreateViewMatrix(Vector3 eye, Vector3 rotation)
+        {
+            return CreateTranslation(-eye.X, -eye.Y, -eye.Z) *
+                Matrix4x4.Transpose(CreateRotation(rotation.X, rotation.Y, rotation.Z));
+        }
+
+        public static Matrix4x4 CreateProjectionBySize(float width, float height, float near, float far)
+        {
+            var v1 = 2 * near / width;
+            var v2 = 2 * near / height;
+            var v3 = far / (near - far);
+            var v4 = near * far / (near - far);
+            return new Matrix4x4(
+                v1, 0, 0, 0,
+                0, v2, 0, 0,
+                0, 0, v3, v4,
+                0, 0, -1, 0);
+        }
+
+        public static Matrix4x4 CreateProjectionByAspect(float aspect, float fov, float near, float far)
+        {
+            var v2 = (float)(1 / Math.Tan(fov / 2));
+            var v1 = (float)((1 / aspect) * v2);
+            var v3 = far / (near - far);
+            var v4 = near * far / (near - far);
+            return new Matrix4x4(
+                v1, 0, 0, 0,
+                0, v2, 0, 0,
+                0, 0, v3, v4,
+                0, 0, -1, 0);
+        }
+
+        public static Matrix4x4 CreateViewportMatrix(float minX, float minY, float width, float height)
+        {
+            var v1 = width / 2;
+            var v2 = height / 2;
+            return new Matrix4x4(
+                v1, 0, 0, minX + v1,
+                0, -v2, 0, minY + v2,
+                0, 0, 1, 0,
+                0, 0, 0, 1);
+        }
+
+        public static Matrix4x4 CreateMatrix(Matrix4x4 viewportMatrix, Matrix4x4 projectionMatrix, Matrix4x4 viewMatrix, Matrix4x4 modelMatrix)
+        {
+            return viewportMatrix * projectionMatrix * viewMatrix * modelMatrix;
         }
     }
 }
