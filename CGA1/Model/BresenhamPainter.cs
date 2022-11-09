7ec5504400899a3df22bea2b3ab801ec78a53012
ZaharPro <zahar7331@gmail.com>
2022-11-09 15:36:33 +0300

    add Painters

---
+++ CGA1/Model/BresenhamPainter.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace CGA1.Model
+{
+    public class BresenhamPainter : IObjPainter
+    {
+        public void Paint(Obj obj, WriteableBitmap bitmap)
+        {
+            var bresenham = new Bresenham(obj, new WritableImage(bitmap));
+            bresenham.DrawModel();
+        }
+    }
+}
---
+++ CGA1/Model/IObjPainter.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace CGA1.Model
+{
+    public interface IObjPainter
+    {
+        void Paint(Obj obj, WriteableBitmap bitmap);
+    }
+}
