21d9a3c3599c8f5a67fd384e56d26585f883de40
ZaharPro <zahar7331@gmail.com>
2022-11-11 22:06:20 +0300

    update SetProperty method

--- CGA1/ViewModel/MainWindowViewModel.cs
+++ CGA1/ViewModel/MainWindowViewModel.cs
@@ -43,39 +43,39 @@ namespace CGA1.ViewModel
         {
             FileDialog = new OpenFileDialog
             {
-                Filter = "Assembly | *.dll",
-                Title = "Select assembly",
+                Filter = "Object | *.obj",
+                Title = "Select obj file",
                 Multiselect = false
             };
             LoadObjCommand = new DelegateCommand(o => LoadObj());
             RepaintCommand= new DelegateCommand(o => Repaint());
         }
 
-        public float ModelPosX { get => _modelPosX; set => SetProperty(ref _modelPosX, value); }
-        public float ModelPosY { get => _modelPosY; set => SetProperty(ref _modelPosY, value); }
-        public float ModelPosZ { get => _modelPosZ; set => SetProperty(ref _modelPosZ, value); }
+        public float ModelPosX { get => _modelPosX; set => SetProperty(ref _modelPosX, value, nameof(ModelPosX)); }
+        public float ModelPosY { get => _modelPosY; set => SetProperty(ref _modelPosY, value, nameof(ModelPosY)); }
+        public float ModelPosZ { get => _modelPosZ; set => SetProperty(ref _modelPosZ, value, nameof(ModelPosZ)); }
 
-        public float ModelYaw { get => _modelYaw; set => SetProperty(ref _modelYaw, value); }
-        public float ModelPitch { get => _modelPitch; set => SetProperty(ref _modelPitch, value); }
-        public float ModelRoll { get => _modelRoll; set => SetProperty(ref _modelRoll, value); }
+        public float ModelYaw { get => _modelYaw; set => SetProperty(ref _modelYaw, value, nameof(ModelYaw)); }
+        public float ModelPitch { get => _modelPitch; set => SetProperty(ref _modelPitch, value, nameof(ModelPitch)); }
+        public float ModelRoll { get => _modelRoll; set => SetProperty(ref _modelRoll, value, nameof(ModelRoll)); }
 
-        public float ModelScale { get => _modelScale; set => SetProperty(ref _modelScale, value); }
+        public float ModelScale { get => _modelScale; set => SetProperty(ref _modelScale, value, nameof(ModelScale)); }
 
-        public float CameraPosX { get => _cameraPosX; set => SetProperty(ref _cameraPosX, value); }
-        public float CameraPosY { get => _cameraPosY; set => SetProperty(ref _cameraPosY, value); }
-        public float CameraPosZ { get => _cameraPosZ; set => SetProperty(ref _cameraPosZ, value); }
+        public float CameraPosX { get => _cameraPosX; set => SetProperty(ref _cameraPosX, value, nameof(CameraPosX)); }
+        public float CameraPosY { get => _cameraPosY; set => SetProperty(ref _cameraPosY, value, nameof(CameraPosY)); }
+        public float CameraPosZ { get => _cameraPosZ; set => SetProperty(ref _cameraPosZ, value, nameof(CameraPosZ)); }
 
-        public float CameraYaw { get => _cameraYaw; set => SetProperty(ref _cameraYaw, value); }
-        public float CameraPitch { get => _cameraPitch; set => SetProperty(ref _cameraPitch, value); }
-        public float CameraRoll { get => _cameraRoll; set => SetProperty(ref _cameraRoll, value); }
+        public float CameraYaw { get => _cameraYaw; set => SetProperty(ref _cameraYaw, value, nameof(CameraYaw)); }
+        public float CameraPitch { get => _cameraPitch; set => SetProperty(ref _cameraPitch, value, nameof(CameraPitch)); }
+        public float CameraRoll { get => _cameraRoll; set => SetProperty(ref _cameraRoll, value, nameof(CameraRoll)); }
 
-        public Color Color { get => _color; set => SetProperty(ref _color, value); }
+        public Color Color { get => _color; set => SetProperty(ref _color, value, nameof(Color)); }
 
-        public IObjPainter ObjPainter { get => _objPainter; set => SetProperty(ref _objPainter, value); }
+        public IObjPainter ObjPainter { get => _objPainter; set => SetProperty(ref _objPainter, value, nameof(ObjPainter)); }
 
-        public Obj Obj { get => _obj; set => SetProperty(ref _obj, value); }
+        public Obj Obj { get => _obj; set => SetProperty(ref _obj, value, nameof(Obj)); }
 
-        public Image Image { get => _image; set => SetProperty(ref _image, value); }
+        public Image Image { get => _image; set => SetProperty(ref _image, value, nameof(Image)); }
 
         public ICommand LoadObjCommand { get; }
 
@@ -85,7 +85,7 @@ namespace CGA1.ViewModel
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        protected bool SetProperty<T>(ref T field, T newValue, string propertyName)
         {
             if (Equals(field, newValue))
             {
